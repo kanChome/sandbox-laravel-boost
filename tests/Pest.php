59362d1de83efd3dc6810c1b0436ecc74e4ddb5f
kanChome <hellbird1998@gmail.com>
2025-08-18 01:44:44 +0900

    fix(ci): テスト失敗の修正

--- tests/Pest.php
+++ tests/Pest.php
@@ -12,8 +12,11 @@
 */
 
 pest()->extend(Tests\TestCase::class)
-    ->use(Illuminate\Foundation\Testing\RefreshDatabase::class)
-    ->in('Feature');
+    ->use(
+        Illuminate\Foundation\Testing\RefreshDatabase::class,
+        Illuminate\Foundation\Testing\DatabaseTransactions::class
+    )
+    ->in('Feature', 'Unit');
 
 /*
 |--------------------------------------------------------------------------
